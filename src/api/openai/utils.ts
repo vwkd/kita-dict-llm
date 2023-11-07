00faf24c0658b8fcdfb3b5b4900a40aabe740726
vwkd <33468089+vwkd@users.noreply.github.com>
2023-11-07 16:30:09 +0400

    openai: fix missing free in encoder

--- src/api/openai/utils.ts
+++ src/api/openai/utils.ts
@@ -8,5 +8,6 @@ const MODEL_NAME = "gpt-3.5-turbo";
 export function countTokens(str: string): number {
   const encoding = encoding_for_model(MODEL_NAME);
   const tokens = encoding.encode(str);
+  encoding.free();
   return tokens.length;
 }
