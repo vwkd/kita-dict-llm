9ae09045ac00b83f55a266ef2881ac81fb866272
vwkd <33468089+vwkd@users.noreply.github.com>
2023-11-07 16:30:09 +0400

    complete/openai/utils: fix missing free in encoder

--- src/complete/openai/utils.ts
+++ src/complete/openai/utils.ts
@@ -8,5 +8,6 @@ const MODEL_NAME = Deno.env.get("OPENAI_MODEL_NAME")!;
 export function countTokens(str: string): number {
   const encoding = encoding_for_model(MODEL_NAME);
   const tokens = encoding.encode(str);
+  encoding.free();
   return tokens.length;
 }
