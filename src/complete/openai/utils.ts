72a07c9649ade78acd72980ae4de9c2728e302bd
vwkd <33468089+vwkd@users.noreply.github.com>
2024-08-10 14:39:45 +0200

    complete/openai/utils: add token count for image

--- src/complete/openai/utils.ts
+++ src/complete/openai/utils.ts
@@ -11,3 +11,56 @@ export function countTokens(str: string): number {
   encoding.free();
   return tokens.length;
 }
+
+/**
+ * Count tokens for image
+ *
+ * from https://platform.openai.com/docs/guides/vision/calculating-costs
+ *
+ * @param width integer width of image in pixels
+ * @param height integer height of image in pixels
+ * @param detailMode low or high detail mode
+ * @returns number of tokens
+ */
+export function countTokensImage(
+  width: number,
+  height: number,
+  detailMode: "low" | "high",
+): number {
+  // 85 tokens always
+  const tokensDefault = 85;
+
+  // default tokens only
+  if (detailMode == "low") {
+    return tokensDefault;
+  }
+
+  let longerSide = Math.max(width, height);
+  let shorterSide = Math.min(width, height);
+
+  // scale down such that longer side is max. 2048px
+  if (longerSide > 2048) {
+    const scaleFactor = 2048 / longerSide;
+    // todo: increase scaleFactor such that shorter side is integer? Or maybe just round shorter side down?
+    shorterSide *= scaleFactor;
+    longerSide *= scaleFactor;
+  }
+
+  // scale down such that shorter side is max. 768px
+  if (shorterSide > 768) {
+    const scaleFactor = 768 / shorterSide;
+    shorterSide *= scaleFactor;
+    // todo: increase scaleFactor such that longer side is integer? Or maybe just round longer side down?
+    longerSide *= scaleFactor;
+  }
+
+  // count number of 512px square tiles
+  const tilesShort = Math.ceil(shorterSide / 512);
+  const tilesLong = Math.ceil(longerSide / 512);
+  const totalTiles = tilesShort * tilesLong;
+
+  // 170 tokens for each tile plus default tokens
+  const tokens = totalTiles * 170 + tokensDefault;
+
+  return tokens;
+}
